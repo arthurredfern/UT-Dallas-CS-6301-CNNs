7a0a1253b08c0c83036eb1fa21f42e2649f3dd9b
Arthur Redfern <42285491+arthurredfern@users.noreply.github.com>
2019-03-05 21:39:22 -0600

    Add files via upload

--- Code/xNNs_Code_02_Vision_Class_CIFAR.py
+++ Code/xNNs_Code_02_Vision_Class_CIFAR.py
@@ -14,6 +14,20 @@
 #    4. Runtime - Change runtime type - Hardware accelerator - GPU
 #    5. Runtime - Run all
 #
+# NOTES
+#
+#    1. The word testing as used here is really validation
+#
+#    2. Future things to experiment with
+#
+#       a. Performance feels very much training data limited so look into more
+#          data augmentation methods; understand how they affect the mean and
+#          std dev of the training vs testing data and if that is an issue
+#
+#       b. Others find that hue seems to be least helpful / unhelpful
+#
+#       c. Consider using the full image (vs center crop only) during testing
+#
 ################################################################################
 
 
@@ -51,15 +65,15 @@ MODEL_LEVEL_2_BLOCKS = 3
 
 # training
 TRAINING_IMAGE_SIZE        = 32
-TRAINING_ZERO_PADDED_SIZE  = 40
+TRAINING_CROP_SIZE         = 28
 TRAINING_SHUFFLE_BUFFER    = 5000
-TRAINING_BATCH_SIZE        = 128
-TRAINING_NUM_EPOCHS        = 72
+TRAINING_BATCH_SIZE        = 32
+TRAINING_NUM_EPOCHS        = 112
 TRAINING_MOMENTUM          = 0.9                    # currently not used
 TRAINING_REGULARIZER_SCALE = 0.1                    # currently not used
-TRAINING_LR_INITIAL        = 0.01
+TRAINING_LR_INITIAL        = 0.001
 TRAINING_LR_SCALE          = 0.1
-TRAINING_LR_EPOCHS         = 64
+TRAINING_LR_EPOCHS         = 48
 TRAINING_LR_STAIRCASE      = True
 TRAINING_MAX_CHECKPOINTS   = 5
 TRAINING_CHECKPOINT_FILE   = './logs/model_{}.ckpt' # currently not used
@@ -71,37 +85,55 @@ TRAINING_CHECKPOINT_FILE   = './logs/model_{}.ckpt' # currently not used
 #
 ################################################################################
 
+# center crop
+def center_crop(image, crop_height, crop_width):
+
+    # image shape
+    shape  = tf.shape(image)
+    height = shape[0]
+    width  = shape[1]
+
+    # center crop top left point
+    amount_to_be_cropped_h = (height - crop_height)
+    crop_top               = amount_to_be_cropped_h // 2
+    amount_to_be_cropped_w = (width - crop_width)
+    crop_left              = amount_to_be_cropped_w // 2
+    
+    # crop
+    return tf.slice(image, [crop_top, crop_left, 0], [crop_height, crop_width, -1])
+
+# pre processing - training
 def pre_processing_train(image, label):
 
     # note: this function operates on 8 bit data then normalizes to a float
+    # some of the randomization operations may change this
 
-    # add a boarder of 0s
-    image = tf.image.resize_image_with_crop_or_pad(image, TRAINING_ZERO_PADDED_SIZE, TRAINING_ZERO_PADDED_SIZE)
-
-    # random horizontal flip
+    # random horizontal flip, hue, contrast, brightness and saturation
     image = tf.image.random_flip_left_right(image)
-    
-    # random hue, contrast, brightness and saturation
     # image = tf.image.random_hue(image, max_delta=0.05)
     # image = tf.image.random_contrast(image, lower=0.3, upper=1.0)
     # image = tf.image.random_brightness(image, max_delta=0.2)
     # image = tf.image.random_saturation(image, lower=0.0, upper=2.0)
     
-    # random crop back to original size
-    image = tf.random_crop(image, size=[TRAINING_IMAGE_SIZE, TRAINING_IMAGE_SIZE, 3])
-
+    # random crop
+    image = tf.random_crop(image, size=[TRAINING_CROP_SIZE, TRAINING_CROP_SIZE, 3])
+    
     # normalization
-    image = (tf.cast(image, tf.float32) - data_mean)/data_std
+    image = tf.math.divide(tf.math.subtract(tf.cast(image, tf.float32), data_mean), data_std)
     
     return image, label
 
+# pre processing - testing
 def pre_processing_test(image, label):
     
     # note: this function operates on 8 bit data then normalizes to a float
     
-    # normalization
-    image = (tf.cast(image, tf.float32) - data_mean)/data_std
+    # center crop
+    image = center_crop(image, TRAINING_CROP_SIZE, TRAINING_CROP_SIZE)
     
+    # normalization
+    image = tf.math.divide(tf.math.subtract(tf.cast(image, tf.float32), data_mean), data_std)
+
     return image, label
 
 
@@ -118,8 +150,8 @@ cifar10 = keras.datasets.cifar10
 (data_train, labels_train), (data_test, labels_test) = cifar10.load_data()
 
 # normalization values
-data_mean = np.mean(data_train, dtype=np.float32)
-data_std  = np.std(data_train, dtype=np.float32)
+data_mean = np.mean(data_train, axis=tuple(range(data_train.ndim - 1)), dtype=np.float32).reshape((1, 1, 3))
+data_std  = np.std(data_train,  axis=tuple(range(data_train.ndim - 1)), dtype=np.float32).reshape((1, 1, 3))
 
 # label typecast
 labels_train = labels_train.astype(np.int32)
@@ -207,7 +239,7 @@ def model_sequential(data, train_state, num_classes):
 #
 ################################################################################
 
-# sequential batch norm model (85.25 %)
+# sequential batch norm model
 def model_sequential_bn(data, train_state, num_classes):
     
     # data
@@ -267,7 +299,9 @@ def model_sequential_bn(data, train_state, num_classes):
 #
 ################################################################################
 
-# resnet model (4-6-3 91.26 %)
+# resnet model
+# 4-6-3 achieves 91.42 % top 1 accuracy with batch size = 32, num epochs = 112, initial learning rate = 0.001, learning rate scale = 0.1 every 48 epochs
+# potentially a little better as training was stopped after 61 epochs
 @autograph.convert()
 def model_resnet(data, train_state, level_0_blocks, level_1_blocks, level_2_blocks, num_classes):
     
@@ -278,12 +312,12 @@ def model_resnet(data, train_state, level_0_blocks, level_1_blocks, level_2_bloc
     fm_id       = tf.layers.conv2d(data, 32, (3, 3), strides=(1, 1), padding='same', data_format='channels_last', dilation_rate=(1, 1), activation=None, use_bias=False)
 
     # encoder - level 0 special bottleneck x1
-    # input:  32 x 32 x 32
+    # input:  32 x 28 x 28
     # filter: 16 x 32 x 1 x 1 / 1
     # filter: 16 x 16 x 3 x 3
     # filter: 64 x 16 x 1 x 1
     # main:   64 x 32 x 1 x 1 / 1
-    # output: 64 x 32 x 32
+    # output: 64 x 28 x 28
     fm_residual = tf.layers.batch_normalization(fm_id, training=train_state)
     fm_residual = tf.nn.relu(fm_residual)
     fm_residual = tf.layers.conv2d(fm_residual, 16, (1, 1), strides=(1, 1), padding='same', data_format='channels_last', dilation_rate=(1, 1), activation=None, use_bias=False)
@@ -297,12 +331,12 @@ def model_resnet(data, train_state, level_0_blocks, level_1_blocks, level_2_bloc
     fm_id       = tf.add(fm_id, fm_residual)
 
     # encoder - level 0 standard bottleneck x(level_0_blocks - 1)
-    # input:  64 x 32 x 32
+    # input:  64 x 28 x 28
     # filter: 16 x 64 x 1 x 1
     # filter: 16 x 16 x 3 x 3
     # filter: 64 x 16 x 1 x 1
     # main:   identity
-    # output: 64 x 32 x 32
+    # output: 64 x 28 x 28
     for block_repeat_0 in range(level_0_blocks - 1):
         fm_residual = tf.layers.batch_normalization(fm_id, training=train_state)
         fm_residual = tf.nn.relu(fm_residual)
@@ -316,12 +350,12 @@ def model_resnet(data, train_state, level_0_blocks, level_1_blocks, level_2_bloc
         fm_id       = tf.add(fm_id, fm_residual)
 
     # encoder - level 1 down sampling bottleneck x1
-    # input:   64 x 32 x 32
+    # input:   64 x 28 x 28
     # filter:  32 x 64 x 1 x 1 / 2
     # filter:  32 x 32 x 3 x 3
     # filter: 128 x 32 x 1 x 1
     # main:   128 x 64 x 1 x 1 / 2
-    # output: 128 x 16 x 16
+    # output: 128 x 14 x 14
     fm_residual = tf.layers.batch_normalization(fm_id, training=train_state)
     fm_residual = tf.nn.relu(fm_residual)
     fm_residual = tf.layers.conv2d(fm_residual,  32, (1, 1), strides=(2, 2), padding='same', data_format='channels_last', dilation_rate=(1, 1), activation=None, use_bias=False)
@@ -335,12 +369,12 @@ def model_resnet(data, train_state, level_0_blocks, level_1_blocks, level_2_bloc
     fm_id       = tf.add(fm_id, fm_residual)
 
     # encoder - level 1 standard bottleneck x(level_1_blocks - 1)
-    # input:  128 x  16 x 16
+    # input:  128 x  14 x 14
     # filter:  32 x 128 x 1 x 1
     # filter:  32 x  32 x 3 x 3
     # filter: 128 x  32 x 1 x 1
     # main:   identity
-    # output: 128 x  16 x 16
+    # output: 128 x  14 x 14
     for block_repeat_1 in range(level_1_blocks - 1):
         fm_residual = tf.layers.batch_normalization(fm_id, training=train_state)
         fm_residual = tf.nn.relu(fm_residual)
@@ -354,12 +388,12 @@ def model_resnet(data, train_state, level_0_blocks, level_1_blocks, level_2_bloc
         fm_id       = tf.add(fm_id, fm_residual)
     
     # encoder - level 2 down sampling bottleneck x1
-    # input:  128 x  16 x 16
+    # input:  128 x  14 x 14
     # filter:  64 x 128 x 1 x 1 / 2
     # filter:  64 x  64 x 3 x 3
     # filter: 256 x  64 x 1 x 1
     # main:   256 x 128 x 1 x 1 / 2
-    # output: 256 x   8 x 8
+    # output: 256 x   7 x 7
     fm_residual = tf.layers.batch_normalization(fm_id, training=train_state)
     fm_residual = tf.nn.relu(fm_residual)
     fm_residual = tf.layers.conv2d(fm_residual,  64, (1, 1), strides=(2, 2), padding='same', data_format='channels_last', dilation_rate=(1, 1), activation=None, use_bias=False)
@@ -373,12 +407,12 @@ def model_resnet(data, train_state, level_0_blocks, level_1_blocks, level_2_bloc
     fm_id       = tf.add(fm_id, fm_residual)
 
     # encoder - level 2 standard bottleneck x(level_2_blocks - 1)
-    # input:  256 x   8 x 8
+    # input:  256 x   7 x 7
     # filter:  64 x 256 x 1 x 1
     # filter:  64 x  64 x 3 x 3
     # filter: 256 x  64 x 1 x 1
     # main:   identity
-    # output: 256 x   8 x 8
+    # output: 256 x   7 x 7
     for block_repeat_2 in range(level_2_blocks - 1):
         fm_residual = tf.layers.batch_normalization(fm_id, training=train_state)
         fm_residual = tf.nn.relu(fm_residual)
@@ -392,8 +426,8 @@ def model_resnet(data, train_state, level_0_blocks, level_1_blocks, level_2_bloc
         fm_id       = tf.add(fm_id, fm_residual)
     
     # encoder - level 2 special block x1
-    # input:  256 x   8 x 8
-    # output: 256 x   8 x 8
+    # input:  256 x   7 x 7
+    # output: 256 x   7 x 7
     fm_id       = tf.layers.batch_normalization(fm_id, training=train_state)
     fm_id       = tf.nn.relu(fm_id)
 
@@ -513,7 +547,7 @@ for batch_index in range(1):
     data_batch, labels_batch = session.run([data, labels])
     
     # normalize to [0, 1]
-    data_batch = ((data_batch*data_std) + data_mean)/255.0;
+    data_batch = ((data_batch*data_std.reshape((1, 1, 1, 3))) + data_mean.reshape((1, 1, 1, 3)))/255.0;
     
     # convert the final saved predictions to labels
     row_start          = batch_index*TRAINING_BATCH_SIZE
