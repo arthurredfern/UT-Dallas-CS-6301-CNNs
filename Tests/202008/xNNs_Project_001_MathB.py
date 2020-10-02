9b465d6b5e4250748fe7dfbd96bbdfe5d70d8c59
arthurredfern <arthurredfern@gmail.com>
2020-10-02 14:17:38 -0500

    Updates

---
+++ Tests/202008/xNNs_Project_001_MathB.py
@@ -0,0 +1,43 @@
+################################################################################
+#
+# LOGISTICS
+#
+#    <Your name as in eLearning>
+#    <Your UT Dallas identifier>
+#
+# FILE
+#
+#    <nn.py | cnn.py | extra.py>
+#
+# DESCRIPTION
+#
+#    MNIST image classification with an xNN written and trained in Python
+#
+# INSTRUCTIONS
+#
+#    1. Go to Google Colaboratory: https://colab.research.google.com/notebooks/welcome.ipynb
+#    2. File - New Python 3 notebook
+#    3. Cut and paste this file into the cell (feel free to divide into multiple cells)
+#    4. Runtime - Run all
+#
+# NOTES
+#
+#    1. A summary of my nn.py code:
+#
+#       <Forward path code summary / highlights>
+#       <Error code summary / highlights>
+#       <Backward path code summary / highlights>
+#       <Weight update code summary / highlights>
+#       <Anything extra to summary / highlight>
+#
+#    2. Accuracy display
+#
+#       <Per epoch display info cut and pasted from your training output>
+#       <Final accuracy cut and pasted from your training output>
+#
+#    3. Performance display
+#
+#       <Total training time cut and pasted from your training output>
+#       <Per layer info (type, input size, output size, parameter size, MACs, ...)>
+#
+################################################################################
