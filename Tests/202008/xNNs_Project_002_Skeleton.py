1290817390fa1a5059057553c358e4c872521486
arthurredfern <arthurredfern@gmail.com>
2020-10-12 20:28:33 -0500

    Updates

---
+++ Tests/202008/xNNs_Project_002_Skeleton.py
@@ -0,0 +1,259 @@
+################################################################################
+#
+# LOGISTICS
+#
+#    Your name as in eLearning
+#    Your UT Dallas identifier
+#
+# DESCRIPTION
+#
+#    Image classification in PyTorch for ImageNet reduced to 100 classes and
+#    down sampled such that the short side is 64 pixels and the long side is
+#    >= 64 pixels
+#
+#    This script achieved a best accuracy of ??.??% on epoch ?? with a learning
+#    rate at that point of ?.????? and time required for each epoch of ~ ??? s
+#
+# INSTRUCTIONS
+#
+#    1. Go to Google Colaboratory: https://colab.research.google.com/notebooks/welcome.ipynb
+#    2. File - New Python 3 notebook
+#    3. Cut and paste this file into the cell (feel free to divide into multiple cells)
+#    4. Runtime - Run all
+#
+# NOTES
+#
+#    0. For a mapping of category names to directory names see:
+#       https://gist.github.com/aaronpolhamus/964a4411c0906315deb9f4a3723aac57
+#
+#    1. The original 2012 ImageNet images are down sampled such that their short
+#       side is 64 pixels (the other side is >= 64 pixels) and only 100 of the
+#       original 1000 classes are kept.
+#
+#    2. Build and train a RegNetX image classifier modified as follows:
+#
+#       - Set stride = 1 (instead of stride = 2) in the stem
+#       - Replace the first stride = 2 down sampling building block in the
+#         original network by a stride = 1 normal building block
+#       - The fully connected layer in the decoder outputs 100 classes instead
+#         of 1000 classes
+#
+#       The original RegNetX takes in 3x224x224 input images and generates Nx7x7
+#       feature maps before the decoder, this modified RegNetX will take in
+#       3x56x56 input images and generate Nx7x7 feature maps before the decoder.
+#       For reference, an implementation of this network took ~ 112 s per epoch
+#       for training, validation and checkpoint saving on Sep 27, 2020 using a
+#       free GPU runtime in Google Colab.
+#
+################################################################################
+
+################################################################################
+#
+# IMPORT
+#
+################################################################################
+
+# torch
+import torch
+import torch.nn       as     nn
+import torch.optim    as     optim
+from   torch.autograd import Function
+
+# torch utils
+import torchvision
+import torchvision.transforms as transforms
+
+# additional libraries
+import os
+import urllib.request
+import zipfile
+import time
+import math
+import numpy             as np
+import matplotlib.pyplot as plt
+
+################################################################################
+#
+# PARAMETERS
+#
+################################################################################
+
+# data
+DATA_DIR_1        = 'data'
+DATA_DIR_2        = 'data/imagenet64'
+DATA_DIR_TRAIN    = 'data/imagenet64/train'
+DATA_DIR_TEST     = 'data/imagenet64/val'
+DATA_FILE_TRAIN_1 = 'Train1.zip'
+DATA_FILE_TRAIN_2 = 'Train2.zip'
+DATA_FILE_TRAIN_3 = 'Train3.zip'
+DATA_FILE_TRAIN_4 = 'Train4.zip'
+DATA_FILE_TRAIN_5 = 'Train5.zip'
+DATA_FILE_TEST_1  = 'Val1.zip'
+DATA_URL_TRAIN_1  = 'https://github.com/arthurredfern/UT-Dallas-CS-6301-CNNs/raw/master/Data/Train1.zip'
+DATA_URL_TRAIN_2  = 'https://github.com/arthurredfern/UT-Dallas-CS-6301-CNNs/raw/master/Data/Train2.zip'
+DATA_URL_TRAIN_3  = 'https://github.com/arthurredfern/UT-Dallas-CS-6301-CNNs/raw/master/Data/Train3.zip'
+DATA_URL_TRAIN_4  = 'https://github.com/arthurredfern/UT-Dallas-CS-6301-CNNs/raw/master/Data/Train4.zip'
+DATA_URL_TRAIN_5  = 'https://github.com/arthurredfern/UT-Dallas-CS-6301-CNNs/raw/master/Data/Train5.zip'
+DATA_URL_TEST_1   = 'https://github.com/arthurredfern/UT-Dallas-CS-6301-CNNs/raw/master/Data/Val1.zip'
+DATA_BATCH_SIZE   = 512
+DATA_NUM_WORKERS  = 4
+DATA_NUM_CHANNELS = 3
+DATA_NUM_CLASSES  = 100
+DATA_RESIZE       = 64
+DATA_CROP         = 56
+DATA_MEAN         = (0.485, 0.456, 0.406)
+DATA_STD_DEV      = (0.229, 0.224, 0.225)
+
+# model
+# add model parameters here
+
+# training
+# add training parameters here
+
+# file
+# add file parameters here
+
+################################################################################
+#
+# DATA
+#
+################################################################################
+
+# create a local directory structure for data storage
+if (os.path.exists(DATA_DIR_1) == False):
+    os.mkdir(DATA_DIR_1)
+if (os.path.exists(DATA_DIR_2) == False):
+    os.mkdir(DATA_DIR_2)
+if (os.path.exists(DATA_DIR_TRAIN) == False):
+    os.mkdir(DATA_DIR_TRAIN)
+if (os.path.exists(DATA_DIR_TEST) == False):
+    os.mkdir(DATA_DIR_TEST)
+
+# download data
+if (os.path.exists(DATA_FILE_TRAIN_1) == False):
+    urllib.request.urlretrieve(DATA_URL_TRAIN_1, DATA_FILE_TRAIN_1)
+if (os.path.exists(DATA_FILE_TRAIN_2) == False):
+    urllib.request.urlretrieve(DATA_URL_TRAIN_2, DATA_FILE_TRAIN_2)
+if (os.path.exists(DATA_FILE_TRAIN_3) == False):
+    urllib.request.urlretrieve(DATA_URL_TRAIN_3, DATA_FILE_TRAIN_3)
+if (os.path.exists(DATA_FILE_TRAIN_4) == False):
+    urllib.request.urlretrieve(DATA_URL_TRAIN_4, DATA_FILE_TRAIN_4)
+if (os.path.exists(DATA_FILE_TRAIN_5) == False):
+    urllib.request.urlretrieve(DATA_URL_TRAIN_5, DATA_FILE_TRAIN_5)
+if (os.path.exists(DATA_FILE_TEST_1) == False):
+    urllib.request.urlretrieve(DATA_URL_TEST_1, DATA_FILE_TEST_1)
+
+# extract data
+with zipfile.ZipFile(DATA_FILE_TRAIN_1, 'r') as zip_ref:
+    zip_ref.extractall(DATA_DIR_TRAIN)
+with zipfile.ZipFile(DATA_FILE_TRAIN_2, 'r') as zip_ref:
+    zip_ref.extractall(DATA_DIR_TRAIN)
+with zipfile.ZipFile(DATA_FILE_TRAIN_3, 'r') as zip_ref:
+    zip_ref.extractall(DATA_DIR_TRAIN)
+with zipfile.ZipFile(DATA_FILE_TRAIN_4, 'r') as zip_ref:
+    zip_ref.extractall(DATA_DIR_TRAIN)
+with zipfile.ZipFile(DATA_FILE_TRAIN_5, 'r') as zip_ref:
+    zip_ref.extractall(DATA_DIR_TRAIN)
+with zipfile.ZipFile(DATA_FILE_TEST_1, 'r') as zip_ref:
+    zip_ref.extractall(DATA_DIR_TEST)
+
+# transforms
+transform_train = transforms.Compose([transforms.RandomResizedCrop(DATA_CROP), transforms.RandomHorizontalFlip(p=0.5), transforms.ToTensor(), transforms.Normalize(DATA_MEAN, DATA_STD_DEV)])
+transform_test  = transforms.Compose([transforms.Resize(DATA_RESIZE), transforms.CenterCrop(DATA_CROP), transforms.ToTensor(), transforms.Normalize(DATA_MEAN, DATA_STD_DEV)])
+
+# data sets
+dataset_train = torchvision.datasets.ImageFolder(DATA_DIR_TRAIN, transform=transform_train)
+dataset_test  = torchvision.datasets.ImageFolder(DATA_DIR_TEST,  transform=transform_test)
+
+# data loader
+dataloader_train = torch.utils.data.DataLoader(dataset_train, batch_size=DATA_BATCH_SIZE, shuffle=True,  num_workers=DATA_NUM_WORKERS, pin_memory=True, drop_last=True)
+dataloader_test  = torch.utils.data.DataLoader(dataset_test,  batch_size=DATA_BATCH_SIZE, shuffle=False, num_workers=DATA_NUM_WORKERS, pin_memory=True, drop_last=True)
+
+################################################################################
+#
+# NETWORK BUILDING BLOCK
+#
+################################################################################
+
+# X block
+class XBlock(nn.Module):
+
+    # initialization
+    def __init__(self, Ni, No, Fr, Fc, Sr, Sc, G):
+
+        # parent initialization
+        super(XBlock, self).__init__()
+
+        # add your code here
+        # operations needed to create a parameterized XBlock
+
+    # forward path
+    def forward(self, x):
+
+        # add your code here
+        # tie together the operations to create a parameterized XBlock
+        y = x
+
+        # return
+        return y
+
+################################################################################
+#
+# NETWORK
+#
+################################################################################
+
+# define
+class Model(nn.Module):
+
+    # initialization
+    def __init__(self,
+                 data_num_channels,
+                 # include model parameters here
+                 data_num_classes):
+
+        # parent initialization
+        super(Model, self).__init__()
+
+        # add your code here
+        # operations needed to create a modified RegNetX-200MF network
+        # use the parameterized XBlock defined to simplify this section
+
+    # forward path
+    def forward(self, x):
+
+        # add your code here
+        # tie together the operations to create a modified RegNetX-200MF
+        y = x
+
+        # return
+        return y
+
+# create
+model = Model(DATA_NUM_CHANNELS,
+              # include model parameters here
+              DATA_NUM_CLASSES)
+
+# enable data parallelization for multi GPU systems
+if (torch.cuda.device_count() > 1):
+    model = nn.DataParallel(model)
+print('Using {0:d} GPU(s)'.format(torch.cuda.device_count()), flush=True)
+
+################################################################################
+#
+# ERROR AND OPTIMIZER
+#
+################################################################################
+
+# add your code here
+# define the error criteria and optimizer
+
+################################################################################
+#
+# TRAINING
+#
+################################################################################
+
+# add your code here
+# perform network training, validation and checkpoint saving
+# see previous examples in the Code directory
